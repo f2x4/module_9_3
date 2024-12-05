3293d9283f53912f022965d6c45b5a8cd1ad039d
f2x4 <77464026+f2x4@users.noreply.github.com>
2024-12-05 20:32:25 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,14 @@
+
+
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (
+    len(first[i]) - len(second[i]) for i in range(len(first))
+    if (len(first[i]) - len(second[i])) != 0
+)
+second_result = (
+    len(first[i]) == len(second[i]) for i in range(len(first))
+)
+print(list(first_result))
+print(list(second_result))
